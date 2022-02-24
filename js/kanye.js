325307a47c7f1d3f87a86e5d83b0f95e49553df5
Shakawath Hossen <shakawath15-2483@diu.edu.bd>
2022-02-24 15:52:10 +0600

    final

---
+++ js/kanye.js
@@ -0,0 +1,12 @@
+const loadQuotes = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuotes(data));
+}
+
+const displayQuotes = (quotes) => {
+    const quoteElement = document.getElementById('quote');
+    quoteElement.innerText = quotes.quote;
+    quoteElement.style.color = 'white';
+    quoteElement.style.fontSize = '18px';
+}
